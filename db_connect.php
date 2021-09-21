a7df658b6cc65fe66f19ea73c24802b7fb639736
Matheus Nascimento Almeida <68080896+MatheusNascimentoA@users.noreply.github.com>
2021-09-21 00:13:48 -0300

    Add files via upload

---
+++ db_connect.php
@@ -0,0 +1,15 @@
+<?php 
+//conexão bd
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "sistema_login";
+
+$connect = mysqli_connect($servername, $username, $password, $db_name);
+
+if(mysqli_connect_error()) {
+	echo "Falha na conexão:".mysqli_connect_error();
+}
+
+
+?>
